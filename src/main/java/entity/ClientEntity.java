4c3eddb76c6a2efa487aa1783c584ba02e7e7ca7
root <rutosha@yandex.ru>
2019-02-22 10:07:21 +0300

    v1

---
+++ src/main/java/entity/ClientEntity.java
@@ -0,0 +1,128 @@
+package entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "client", schema = "public", catalog = "bsbd_homework")
+public class ClientEntity {
+    private int clientId;
+    private String surname;
+    private String name;
+    private String patronymic;
+    private int telephoneNumber;
+    private String adress;
+    private int okpo;
+    private int okpoId;
+
+    @Id
+    @Column(name = "client_id", nullable = false)
+    public int getClientId() {
+        return clientId;
+    }
+
+    public void setClientId(int clientId) {
+        this.clientId = clientId;
+    }
+
+    @Basic
+    @Column(name = "surname", nullable = false, length = 100)
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    @Basic
+    @Column(name = "name", nullable = false, length = 100)
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Basic
+    @Column(name = "patronymic", nullable = false, length = 100)
+    public String getPatronymic() {
+        return patronymic;
+    }
+
+    public void setPatronymic(String patronymic) {
+        this.patronymic = patronymic;
+    }
+
+    @Basic
+    @Column(name = "telephone_number", nullable = false)
+    public int getTelephoneNumber() {
+        return telephoneNumber;
+    }
+
+    public void setTelephoneNumber(int telephoneNumber) {
+        this.telephoneNumber = telephoneNumber;
+    }
+
+    @Basic
+    @Column(name = "adress", nullable = false, length = 1000)
+    public String getAdress() {
+        return adress;
+    }
+
+    public void setAdress(String adress) {
+        this.adress = adress;
+    }
+
+    @Basic
+    @Column(name = "okpo", nullable = false)
+    public int getOkpo() {
+        return okpo;
+    }
+
+    public void setOkpo(int okpo) {
+        this.okpo = okpo;
+    }
+
+    @Basic
+    @Column(name = "okpo_id", nullable = false)
+    public int getOkpoId() {
+        return okpoId;
+    }
+
+    public void setOkpoId(int okpoId) {
+        this.okpoId = okpoId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        ClientEntity that = (ClientEntity) o;
+
+        if (clientId != that.clientId) return false;
+        if (telephoneNumber != that.telephoneNumber) return false;
+        if (okpo != that.okpo) return false;
+        if (okpoId != that.okpoId) return false;
+        if (surname != null ? !surname.equals(that.surname) : that.surname != null) return false;
+        if (name != null ? !name.equals(that.name) : that.name != null) return false;
+        if (patronymic != null ? !patronymic.equals(that.patronymic) : that.patronymic != null) return false;
+        if (adress != null ? !adress.equals(that.adress) : that.adress != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = clientId;
+        result = 31 * result + (surname != null ? surname.hashCode() : 0);
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        result = 31 * result + (patronymic != null ? patronymic.hashCode() : 0);
+        result = 31 * result + telephoneNumber;
+        result = 31 * result + (adress != null ? adress.hashCode() : 0);
+        result = 31 * result + okpo;
+        result = 31 * result + okpoId;
+        return result;
+    }
+}
